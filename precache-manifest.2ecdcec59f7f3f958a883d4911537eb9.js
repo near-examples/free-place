828980454ced17ca5283f6cadf00371fbf6de986
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-08-21 12:01:54 -0700

    Updates

---
+++ precache-manifest.2ecdcec59f7f3f958a883d4911537eb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed39f5d79bfd57e4837f2a37269cba6e",
+    "url": "/free-place/index.html"
+  },
+  {
+    "revision": "705b274d117963375780",
+    "url": "/free-place/static/css/main.27264ffc.chunk.css"
+  },
+  {
+    "revision": "b5f4d357ac19929be849",
+    "url": "/free-place/static/js/2.09657a69.chunk.js"
+  },
+  {
+    "revision": "a8659539666f01faf7a39edf6361ec26",
+    "url": "/free-place/static/js/2.09657a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705b274d117963375780",
+    "url": "/free-place/static/js/main.61446ec4.chunk.js"
+  },
+  {
+    "revision": "01b01763e25430f49963",
+    "url": "/free-place/static/js/runtime-main.ab2903ee.js"
+  }
+]);
